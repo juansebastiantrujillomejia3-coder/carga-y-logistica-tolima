ed2e6873d60609532f2137c021175464406073f9
tu nombre <tu-correo@ejemplo.com>
2026-03-17 22:06:18 -0500

    se agrego un logo y se modificaron varias cosas. register,datadase

--- carga-y-logistica-tolima/app/Http/Controllers/Auth/RegisteredUserController.php
+++ carga-y-logistica-tolima/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -29,17 +29,19 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+       $request->validate([
+    'name' => ['required', 'string', 'max:255'],
+    'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+    'password' => ['required', 'confirmed', Rules\Password::defaults()],
+    'role_id' => ['required', 'exists:roles,id'],
+]);
 
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+    'name' => $request->name,
+    'email' => $request->email,
+    'password' => Hash::make($request->password),
+    'role_id' => $request->role_id,
+]);
 
         event(new Registered($user));
 
